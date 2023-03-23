57c306f3f60f6f001eb9eeaf71c2b3e8d8576ea7
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2023-03-23 23:57:34 +0900

    update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using System.Net;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public RawImage img;
+
+    private LocationService GPS;
+    private float lat, lng;
+
+    void Start()
+    {
+        GPS = Input.location;
+
+        if (GPS.isEnabledByUser)
+        {
+            // GPSの測位を開始する
+            GPS.Start();
+
+            // 5秒おきにGPS情報を取得する
+            InvokeRepeating("GetLocation", 0, 5);
+        }
+        else
+        {
+            // GPSにアクセスできない
+            label.text = "GPS不許可";
+        }
+
+        // まずはダミー位置情報で読み込む
+        lat = 35.686275f;
+        lng = 139.752835f;
+        StartCoroutine(GetMapImage());
+    }
+
+    //アプリ終了時に呼ばれるメソッド
+    void OnDisable()
+    {
+        // アプリ終了時にGPSを停止する（電池節約）
+        GPS.Stop();
+    }
+
+    //位置情報更新用のメソッド
+    void GetLocation()
+    {
+        switch (GPS.status)
+        {
+            case LocationServiceStatus.Failed:
+                // GPS取得失敗
+                label.text = "取得失敗";
+                CancelInvoke("GetLocation");
+                break;
+
+            case LocationServiceStatus.Initializing:
+                // まだ準備中
+                label.text = "取得中…";
+                break;
+
+            case LocationServiceStatus.Running:
+                // GPS利用可能
+                var data = GPS.lastData;
+                if (lat != data.latitude || lng != data.longitude)
+                {
+                    // 前回と位置が違うなら地図更新
+                    lat = data.latitude;
+                    lng = data.longitude;
+                    StartCoroutine(GetMapImage());
+                }
+                break;
+        }
+    }
+
+    //地図を取得するメソッド
+    IEnumerator GetMapImage()
+    {
+        label.text = string.Format("w:{0:f6}\nh:{1:f6}", lat, lng);
+
+        // URLを生成
+        var url = $"https://a.tile.openstreetmap.org/1/{lat}/{lng}.png";
+        Debug.Log(url);
+
+        //API
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        //urlに接続してデータが帰ってくるまで待機状態にする。
+        yield return www.SendWebRequest();
+        //エラー確認
+        switch (www.result)
+        {
+            case UnityWebRequest.Result.InProgress:
+                Debug.Log("リクエスト中");
+                break;
+            case UnityWebRequest.Result.Success:
+                //成功
+                Debug.Log(www.downloadHandler.text);
+                // 画像として読み込む
+                img.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+                break;
+        }
+    }
+}
+
